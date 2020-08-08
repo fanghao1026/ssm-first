79a703dd17766e4a3c0f761dc51970c93d74071c
fanghaoo <303592939@qq.com>
2020-08-08 17:33:28 +0800

    Initial commit

---
+++ src/main/java/a.java
@@ -0,0 +1,66 @@
+import jdk.internal.org.objectweb.asm.commons.AdviceAdapter;
+
+import javax.swing.text.html.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author FH
+ * @create 2020-08-08
+ */
+public class a {
+
+    public static void main(String[] args) {
+
+        private String ada = AdviceAdapter ;
+
+        private String dada = AdviceAdapter ;
+        
+        private int dada = dada;
+        private String  =  ;
+        
+        
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("a.main");
+        System.out.println("true = " + true);
+
+//       fori
+        for (int i = 0; i <  ; i++) {
+
+        }
+//iter
+        for (String arg : args) {
+
+        }
+        public void testdad(){
+            adad
+            }
+
+        public void testdada(){
+            AdviceAdapter
+        }
+
+        if (args == null) {
+
+        }
+        if (args != null) {
+            
+        }
+
+        ArrayList arrayList = new ArrayList();
+        for (Object o : arrayList) {
+
+        }
+        for (int i = 0; i < arrayList.size(); i++) {
+
+        }
+        for (int i = arrayList.size() - 1; i >= 0; i--) {
+
+        }
+
+        if (arrayList == null) {
+
+        }
+
+    }
+}
